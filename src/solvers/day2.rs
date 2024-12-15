0210dc28768ad4738a3d91e84181199598146b2b
Gerald Lee <teumessish@gmail.com>
2024-12-14 22:24:37 -0500

    Day 2 Part 2

--- src/solvers/day2.rs
+++ src/solvers/day2.rs
@@ -3,29 +3,33 @@ pub struct Solver {}
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
         let mut num_safe: usize = 0;
-        'report: for report in input.lines() {
-            let mut prev: Option<usize> = None;
-            let mut increasing = false;
-            let mut decreasing = false;
-            for level in report.split(" ").map(|str_level| str_level.parse::<usize>().unwrap()) {
-                if let Some(prev) = prev {
-                    if prev > level {
-                        decreasing = true;
+        for report in input.lines() {
+            let levels: Vec<usize> = report.split(" ").map(|str_level| str_level.parse::<usize>().unwrap()).collect();
+            'skip: for skip in 0..levels.len() {
+                let mut prev: Option<usize> = None;
+                let mut increasing = false;
+                let mut decreasing = false;
+                for level in levels.iter().enumerate().filter(|(i, _)| *i != skip).map(|(_, v)| *v) {
+                    if let Some(prev) = prev {
+                        if prev > level {
+                            decreasing = true;
+                        }
+                        else if prev < level {
+                            increasing = true;
+                        }
+                        let diff = prev.abs_diff(level);
+                        if diff < 1 || diff > 3 {
+                            continue 'skip;
+                        }
+                        if increasing && decreasing {
+                            continue 'skip;
+                        }
                     }
-                    else if prev < level {
-                        increasing = true;
-                    }
-                    let diff = prev.abs_diff(level);
-                    if diff < 1 || diff > 3 {
-                        continue 'report;
-                    }
-                    if increasing && decreasing {
-                        continue 'report;
-                    }
+                    prev = Some(level);
                 }
-                prev = Some(level);
+                num_safe += 1;
+                break;
             }
-            num_safe += 1;
         }
         num_safe.to_string()
     }
