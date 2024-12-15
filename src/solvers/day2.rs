46547358216fc51885044af1df477eeb3a88a050
Gerald Lee <teumessish@gmail.com>
2024-12-14 22:04:46 -0500

    Day 2 Part 1

--- src/solvers/day2.rs
+++ src/solvers/day2.rs
@@ -2,7 +2,31 @@ pub struct Solver {}
 
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        // TODO: Implement
-        input.to_string()
+        let mut num_safe: usize = 0;
+        'report: for report in input.lines() {
+            let mut prev: Option<usize> = None;
+            let mut increasing = false;
+            let mut decreasing = false;
+            for level in report.split(" ").map(|str_level| str_level.parse::<usize>().unwrap()) {
+                if let Some(prev) = prev {
+                    if prev > level {
+                        decreasing = true;
+                    }
+                    else if prev < level {
+                        increasing = true;
+                    }
+                    let diff = prev.abs_diff(level);
+                    if diff < 1 || diff > 3 {
+                        continue 'report;
+                    }
+                    if increasing && decreasing {
+                        continue 'report;
+                    }
+                }
+                prev = Some(level);
+            }
+            num_safe += 1;
+        }
+        num_safe.to_string()
     }
 }
