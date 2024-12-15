473f9e385205942aa707bdf4b51d80d1327eed53
Gerald Lee <teumessish@gmail.com>
2024-12-14 22:59:06 -0500

    Day 3 Part 2

--- src/solvers/day3.rs
+++ src/solvers/day3.rs
@@ -4,10 +4,20 @@ pub struct Solver {}
 
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+        let re = Regex::new(r"(mul\(([0-9]+),([0-9]+)\))|(do\(\))|(don't\(\))").unwrap();
         let mut acc: usize = 0;
+        let mut enabled = true;
         for mat in re.captures_iter(input) {
-            acc += mat.get(1).unwrap().as_str().parse::<usize>().unwrap() * mat.get(2).unwrap().as_str().parse::<usize>().unwrap();
+            let full = mat.get(0).unwrap().as_str();
+            if full.starts_with("mul") && enabled {
+                acc += mat.get(2).unwrap().as_str().parse::<usize>().unwrap() * mat.get(3).unwrap().as_str().parse::<usize>().unwrap();
+            }
+            else if full.starts_with("don't") {
+                enabled = false;
+            }
+            else if full.starts_with("do") {
+                enabled = true;
+            }
         }
         acc.to_string()
     }
