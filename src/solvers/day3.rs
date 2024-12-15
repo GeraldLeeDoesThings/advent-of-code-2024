ca9673097f003b58f67a9bd903cf589aefaa234c
Gerald Lee <teumessish@gmail.com>
2024-12-14 22:48:00 -0500

    Day 3 Part 1

--- src/solvers/day3.rs
+++ src/solvers/day3.rs
@@ -1,8 +1,14 @@
+use regex::Regex;
+
 pub struct Solver {}
 
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        // TODO: Implement
-        input.to_string()
+        let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+        let mut acc: usize = 0;
+        for mat in re.captures_iter(input) {
+            acc += mat.get(1).unwrap().as_str().parse::<usize>().unwrap() * mat.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        }
+        acc.to_string()
     }
 }
