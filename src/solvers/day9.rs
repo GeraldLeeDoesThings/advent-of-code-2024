64e4e5510f8846213e0985f5a266933413d9f206
Gerald Lee <teumessish@gmail.com>
2024-12-19 01:56:16 -0500

    Day 9 Part 2

--- src/solvers/day9.rs
+++ src/solvers/day9.rs
@@ -4,6 +4,7 @@ impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
         let mut count: u32 = 0;
         let mut empty = false;
+        let mut gaps: Vec<(usize, usize)> = Vec::new();
         let mut fs: Vec<Option<u32>> = input
             .chars()
             .flat_map(|c| {
@@ -18,28 +19,92 @@ impl crate::Solver for Solver {
                 }
             })
             .collect();
+
         let mut low: usize = 0;
-        let mut high = fs.len() - 1;
-        let mut acc: usize = 0;
-        while low < fs.len() {
-            while high > 0 && fs.get(high).unwrap().is_none() {
-                high -= 1;
+
+        fn dump_fs(data: &Vec<Option<u32>>) {
+            for d in data {
+                match d {
+                    None => print!("."),
+                    Some(v) => print!("{v}"),
+                }
             }
-            match fs.get(low).unwrap() {
-                None => {
-                    if high > low {
-                        fs.swap(low, high)
-                    } else {
-                        low += 1;
-                    }
-                }
-                &Some(v) => {
-                    acc += low * v as usize;
-                    low += 1;
-                }
+            println!()
+        }
+
+        fn count_free_space_at(data: &Vec<Option<u32>>, i: usize) -> usize {
+            let mut found: usize = 0;
+            while data.get(i + found).map(|v| v.is_none()).unwrap_or(false) {
+                found += 1;
+            }
+            found
+        }
+
+        while low < fs.len() {
+            if fs[low].is_none() {
+                let space = count_free_space_at(&fs, low);
+                gaps.push((low, space));
+                low += space;
+            } else {
+                low += 1;
             }
         }
-        println!("");
+
+        let mut high = fs.len() - 1;
+
+        // Counts backwards...
+        fn count_block_size_at(data: &Vec<Option<u32>>, i: usize) -> usize {
+            let mut found: usize = 1;
+            let target = data.get(i).unwrap().unwrap();
+            while found <= i
+                && data
+                    .get(i - found)
+                    .map(|v| v.map(|iv| iv == target))
+                    .flatten()
+                    .unwrap_or(false)
+            {
+                found += 1;
+            }
+            found
+        }
+
+        low = 0;
+        'defrag: while high > low {
+            if fs[high].is_some() {
+                let space = count_block_size_at(&fs, high);
+                for (i, (gap_index, gap_size)) in gaps.iter().enumerate() {
+                    if *gap_index > high - (space - 1) {
+                        break;
+                    }
+                    if *gap_size >= space {
+                        for i in 0..space {
+                            fs.swap(gap_index + i, high - i);
+                        }
+
+                        if *gap_size == space {
+                            gaps.remove(i);
+                        } else {
+                            gaps[i].0 += space;
+                            gaps[i].1 -= space;
+                        }
+                        high -= space;
+                        continue 'defrag;
+                    }
+                }
+                if space > high {
+                    break 'defrag;
+                }
+                high -= space;
+            } else {
+                high -= 1;
+            }
+        }
+
+        let acc: usize = fs
+            .iter()
+            .enumerate()
+            .map(|(i, b)| i * b.unwrap_or(0) as usize)
+            .sum();
         acc.to_string()
     }
 }
