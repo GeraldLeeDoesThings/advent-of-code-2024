1d3a951d446ad2997b1ab29ee29c3227b348afca
Gerald Lee <teumessish@gmail.com>
2024-12-21 03:02:09 -0500

    Day 11 Part 2

--- src/solvers/day11.rs
+++ src/solvers/day11.rs
@@ -1,10 +1,40 @@
-use std::collections::HashMap;
+use std::{
+    cmp::min,
+    collections::{BinaryHeap, HashMap},
+};
 
 pub struct Solver {}
 
+#[derive(PartialEq, Eq)]
+struct EvolvePair {
+    steps_needed: usize,
+    val: usize,
+    weight: usize,
+}
+
+impl PartialOrd for EvolvePair {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for EvolvePair {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.steps_needed.cmp(&other.steps_needed) {
+            std::cmp::Ordering::Less => std::cmp::Ordering::Greater,
+            std::cmp::Ordering::Equal => match self.val.cmp(&other.val) {
+                std::cmp::Ordering::Less => std::cmp::Ordering::Less,
+                std::cmp::Ordering::Equal => self.weight.cmp(&other.weight),
+                std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
+            },
+            std::cmp::Ordering::Greater => std::cmp::Ordering::Less,
+        }
+    }
+}
+
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        let mut arrangement: Vec<usize> = input
+        let arrangement: Vec<usize> = input
             .split(" ")
             .map(|num_str| num_str.parse().unwrap())
             .collect();
@@ -64,19 +94,38 @@ impl crate::Solver for Solver {
             }
         }
 
-        let solution_iters = Vec::from_iter(
-            arrangement
-                .iter()
-                .map(|stone| evolve(*stone, 25, &mut cache)),
-        );
-        arrangement = solution_iters
-            .iter()
-            .map(|inner| inner.iter())
-            .flatten()
-            .map(|s| *s)
-            .collect();
+        let mut compute_queue = BinaryHeap::from_iter(arrangement.iter().map(|s| EvolvePair {
+            steps_needed: 75,
+            val: *s,
+            weight: 1,
+        }));
+        let mut acc = 0;
+        let limit = 25;
+
+        while let Some(to_comp) = compute_queue.pop() {
+            let target_steps = min(to_comp.steps_needed, limit);
+            let partial = evolve(to_comp.val, target_steps, &mut cache);
+            // println!("{} {} {}", to_comp.steps_needed, to_comp.val, to_comp.weight);
+            if target_steps == to_comp.steps_needed {
+                acc += to_comp.weight * partial.len();
+                // println!("{:#?}", partial);
+            } else {
+                let mut count: HashMap<usize, usize> = HashMap::new();
+                for s in partial {
+                    let current = count.get(&s).unwrap_or(&0);
+                    count.insert(s, current + 1);
+                }
+                for (val, weight) in count {
+                    compute_queue.push(EvolvePair {
+                        steps_needed: to_comp.steps_needed - target_steps,
+                        val: val,
+                        weight: to_comp.weight * weight,
+                    });
+                }
+            }
+        }
 
         // println!("{:#?}", arrangement);
-        arrangement.len().to_string()
+        acc.to_string()
     }
 }
