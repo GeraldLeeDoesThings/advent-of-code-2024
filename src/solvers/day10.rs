8ccfb7421d2daf0ae38cdcf17a5e2c8d718dfa72
Gerald Lee <teumessish@gmail.com>
2024-12-21 00:14:11 -0500

    Day 10 Part 2

--- src/solvers/day10.rs
+++ src/solvers/day10.rs
@@ -11,19 +11,14 @@ impl crate::Solver for Solver {
                 .collect()
         }));
         let mut num_trailheads: usize = 0;
-        let mut ways_to_reach: Vec<Vec<Option<HashSet<usize>>>> =
-            Vec::from_iter(map.iter().map(|v| {
-                v.iter()
-                    .map(|h| match *h {
-                        9 => {
-                            let single = HashSet::from([num_trailheads]);
-                            num_trailheads += 1;
-                            Some(single)
-                        }
-                        _ => None,
-                    })
-                    .collect()
-            }));
+        let mut ways_to_reach: Vec<Vec<Option<usize>>> = Vec::from_iter(map.iter().map(|v| {
+            v.iter()
+                .map(|h| match *h {
+                    9 => Some(1),
+                    _ => None,
+                })
+                .collect()
+        }));
         for (row_index, row) in map.iter().enumerate() {
             for (col, h) in row.iter().enumerate() {
                 if *h == 0 {
@@ -35,41 +30,40 @@ impl crate::Solver for Solver {
         fn search(
             at: (usize, usize),
             map: &Vec<Vec<u8>>,
-            cache: &mut Vec<Vec<Option<HashSet<usize>>>>,
-        ) -> HashSet<usize> {
+            cache: &mut Vec<Vec<Option<usize>>>,
+        ) -> usize {
             let diffs: [(isize, isize); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
 
             if let Some(cached) = cache
                 .get(at.0)
-                .map(|v| v.get(at.1))
+                .map(|v| v.get(at.1).map(|inner| *inner))
                 .flatten()
-                .map(|inner| inner.clone())
                 .flatten()
             {
-                return cached.clone();
+                return cached;
             }
 
             let current_height = map[at.0][at.1];
-            let mut total_ways: HashSet<usize> = HashSet::new();
+            let mut total_ways: usize = 0;
             for (dy, dx) in diffs {
                 if let (Some(ny), Some(nx)) =
                     (at.0.checked_add_signed(dx), at.1.checked_add_signed(dy))
                 {
                     if let Some(height) = map.get(ny).map(|row| row.get(nx).map(|h| *h)).flatten() {
                         if height == current_height + 1 {
-                            total_ways.extend(search((ny, nx), map, cache));
+                            total_ways += search((ny, nx), map, cache);
                         }
                     }
                 }
             }
-            cache[at.0][at.1] = Some(total_ways.clone());
+            cache[at.0][at.1] = Some(total_ways);
 
             total_ways
         }
 
         let acc: usize = trailheads
             .iter()
-            .map(|pos| search(*pos, &map, &mut ways_to_reach).len())
+            .map(|pos| search(*pos, &map, &mut ways_to_reach))
             .sum();
         acc.to_string()
     }
