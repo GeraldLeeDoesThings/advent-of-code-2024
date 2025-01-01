51eb4fb0f481dd76079063486e13f2c7dd8cdd40
Gerald Lee <teumessish@gmail.com>
2025-01-01 17:34:55 -0500

    Day 22 Part 1

--- src/solvers/day22.rs
+++ src/solvers/day22.rs
@@ -1,8 +1,23 @@
 pub struct Solver {}
 
+fn next_secret(secret: usize) -> usize {
+    let mut new_secret = secret ^ (secret << 6) % 16777216;
+    new_secret = new_secret ^ (new_secret >> 5) % 16777216;
+    new_secret ^ (new_secret << 11) % 16777216
+}
+
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        // TODO: Implement
-        input.to_string()
+        input
+            .lines()
+            .map(|line| {
+                let mut secret: usize = line.parse().unwrap();
+                for _ in 0..2000 {
+                    secret = next_secret(secret);
+                }
+                secret
+            })
+            .sum::<usize>()
+            .to_string()
     }
 }
