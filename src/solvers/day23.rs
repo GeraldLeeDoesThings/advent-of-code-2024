d4097a29374495b8a5c605e103375a14ef5a6431
Gerald Lee <teumessish@gmail.com>
2025-01-02 05:31:44 -0500

    Day 23 Part 2

--- src/solvers/day23.rs
+++ src/solvers/day23.rs
@@ -1,11 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 pub struct Solver {}
 
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
         let mut computers: HashSet<String> = HashSet::new();
-        let mut connections: HashMap<String, HashSet<String>> = HashMap::new();
+        let mut connections: HashMap<String, BTreeSet<String>> = HashMap::new();
 
         for line in input.lines() {
             let mut parts = line.split('-');
@@ -17,33 +17,50 @@ impl crate::Solver for Solver {
             if let Some(conns_a) = connections.get_mut(&comp_a) {
                 conns_a.insert(comp_b.clone());
             } else {
-                connections.insert(comp_a.clone(), HashSet::from([comp_b.clone()]));
+                connections.insert(comp_a.clone(), BTreeSet::from([comp_b.clone()]));
             }
 
             if let Some(conns_b) = connections.get_mut(&comp_b) {
                 conns_b.insert(comp_a);
             } else {
-                connections.insert(comp_b, HashSet::from([comp_a]));
+                connections.insert(comp_b, BTreeSet::from([comp_a]));
             }
         }
 
-        let mut triplets: HashSet<[String; 3]> = HashSet::new();
+        let mut cliques: BTreeSet<BTreeSet<String>> = BTreeSet::from_iter(
+            computers
+                .iter()
+                .map(|computer| BTreeSet::from_iter([computer.clone()])),
+        );
+        let mut seen_cliques: HashSet<BTreeSet<String>> = HashSet::new();
+        let mut maximal_cliques: HashSet<BTreeSet<String>> = HashSet::new();
 
-        for comp_a in &computers {
-            let a_conns = connections.get(comp_a).unwrap();
-            for comp_b in a_conns {
-                let b_conns = connections.get(comp_b).unwrap();
-                for mutual in a_conns.intersection(b_conns) {
-                    let mut triplet = [comp_a.clone(), comp_b.clone(), mutual.clone()];
-                    triplet.sort();
-                    triplets.insert(triplet);
+        // Fast enough clique finding algorithm
+        while let Some(clique) = cliques.pop_last() {
+            let mut found_any = false;
+            for comp in &computers {
+                let connected = connections.get(comp).unwrap();
+                if clique.is_subset(connected) && !clique.contains(comp) {
+                    let mut expanded = clique.clone();
+                    expanded.insert(comp.clone());
+                    if !seen_cliques.contains(&expanded) {
+                        found_any = true;
+                        seen_cliques.insert(expanded.clone());
+                        cliques.insert(expanded);
+                    }
                 }
             }
+            if !found_any {
+                maximal_cliques.insert(clique);
+            }
         }
-        triplets
+        maximal_cliques
             .iter()
-            .filter(|triplet| triplet.iter().any(|comp| comp.starts_with('t')))
-            .count()
-            .to_string()
+            .max_by_key(|clique| clique.len())
+            .unwrap()
+            .iter()
+            .map(|computer| computer.clone())
+            .collect::<Vec<String>>()
+            .join(",")
     }
 }
