dc7ed30ae990d0cc0256a5c0589cdef7b4e9d5b2
Gerald Lee <teumessish@gmail.com>
2025-01-01 20:42:44 -0500

    Day 22 Part 2

--- src/solvers/day22.rs
+++ src/solvers/day22.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, isize};
+
 pub struct Solver {}
 
 fn next_secret(secret: usize) -> usize {
@@ -6,18 +8,86 @@ fn next_secret(secret: usize) -> usize {
     new_secret ^ (new_secret << 11) % 16777216
 }
 
+fn last_digit(secret: usize) -> isize {
+    (secret - (secret / 10) * 10) as isize
+}
+
+struct Secret {
+    value: usize,
+}
+
+impl Iterator for Secret {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.value;
+        self.value = next_secret(self.value);
+        Some(result)
+    }
+}
+
+struct SequenceCounter {
+    max: usize,
+    counts: [[[[usize; 19]; 19]; 19]; 19],
+}
+
+impl SequenceCounter {
+    fn new() -> SequenceCounter {
+        Self {
+            max: 0,
+            counts: [[[[0; 19]; 19]; 19]; 19],
+        }
+    }
+
+    fn observe(&mut self, v1: isize, v2: isize, v3: isize, v4: isize, price: usize) {
+        let i: [usize; 4] = [v1, v2, v3, v4].map(|v| (v + 9) as usize);
+        self.counts[i[0]][i[1]][i[2]][i[3]] += price;
+        self.max = self.counts[i[0]][i[1]][i[2]][i[3]].max(self.max);
+    }
+}
+
 impl crate::Solver for Solver {
     fn solve(&self, input: &String) -> String {
-        input
+        let mut counter = SequenceCounter::new();
+        let prices: Vec<Vec<u8>> = input
             .lines()
             .map(|line| {
-                let mut secret: usize = line.parse().unwrap();
-                for _ in 0..2000 {
-                    secret = next_secret(secret);
-                }
-                secret
+                let secret: Secret = Secret {
+                    value: line.parse().unwrap(),
+                };
+                secret.take(2001).map(|v| last_digit(v) as u8).collect()
             })
-            .sum::<usize>()
-            .to_string()
+            .collect();
+
+        let price_diffs: Vec<Vec<i8>> = prices
+            .iter()
+            .map(|prices| {
+                prices
+                    .windows(2)
+                    .map(|pair| pair[1] as i8 - pair[0] as i8)
+                    .collect()
+            })
+            .collect();
+
+        for (single_price, diffs) in prices.iter().zip(price_diffs) {
+            let mut seen = HashSet::new();
+            for (price, diff) in single_price.iter().skip(4).zip(diffs.windows(4)) {
+                assert!(diff.len() == 4);
+                let diffs = (diff[0], diff[1], diff[2], diff[3]);
+                if seen.contains(&diffs) {
+                    continue;
+                }
+                seen.insert(diffs);
+                counter.observe(
+                    diff[0].into(),
+                    diff[1].into(),
+                    diff[2].into(),
+                    diff[3].into(),
+                    *price as usize,
+                );
+            }
+        }
+
+        counter.max.to_string()
     }
 }
